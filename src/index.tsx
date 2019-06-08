be4abad8e7e5958f3bc1a8999bb2ed8ee77ea624
Jamie Bertram <jhbertra@gmail.com>
2019-06-08 13:29:20 -0400

    Register OpenSans font from CDN

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,89 @@
 import * as React from "react";
 import * as ReactDom from "react-dom";
 import {
+    Document,
+    Font,
+    Page,
     PDFViewer,
-    Document
+    StyleSheet
 } from "@react-pdf/renderer";
 
-ReactDom.render(
-    <PDFViewer><Document></Document></PDFViewer>,
-    document.getElementById("root")
-);
+
+function main() {
+    registerFonts();
+
+    const styles = StyleSheet.create({
+        page: {
+            fontFamily: 'OpenSans'
+        }
+    });
+
+    ReactDom.render(
+        <PDFViewer>
+            <Document>
+                <Page size="A4" style={styles.page}>
+                </Page>
+            </Document>
+        </PDFViewer>,
+        document.getElementById("root"));
+}
+
+
+function registerFonts() {
+    const fontsRoot = "https://cdn.jsdelivr.net/npm/open-sans-all@0.1.3/fonts";
+    Font.register({
+        family: "OpenSans",
+        src: `${fontsRoot}/open-sans-300.ttf`,
+        fontWeight: 300
+    });
+    Font.register({
+        family: "OpenSans",
+        src: `${fontsRoot}/open-sans-300italic.ttf`,
+        fontStyle: "italic",
+        fontWeight: 300
+    });
+    Font.register({
+        family: "OpenSans",
+        src: `${fontsRoot}/open-sans-regular.ttf`
+    });
+    Font.register({
+        family: "OpenSans",
+        src: `${fontsRoot}/open-sans-italic.ttf`,
+        fontStyle: "italic"
+    });
+    Font.register({
+        family: "OpenSans",
+        src: `${fontsRoot}/open-sans-600.ttf`,
+        fontWeight: 600
+    });
+    Font.register({
+        family: "OpenSans",
+        src: `${fontsRoot}/open-sans-600italic.ttf`,
+        fontStyle: "italic",
+        fontWeight: 600
+    });
+    Font.register({
+        family: "OpenSans",
+        src: `${fontsRoot}/open-sans-700.ttf`,
+        fontWeight: 700
+    });
+    Font.register({
+        family: "OpenSans",
+        src: `${fontsRoot}/open-sans-700italic.ttf`,
+        fontStyle: "italic",
+        fontWeight: 700
+    });
+    Font.register({
+        family: "OpenSans",
+        src: `${fontsRoot}/open-sans-800.ttf`,
+        fontWeight: 800
+    });
+    Font.register({
+        family: "OpenSans",
+        src: `${fontsRoot}/open-sans-800italic.ttf`,
+        fontStyle: "italic",
+        fontWeight: 800
+    });
+}
+
+main();
